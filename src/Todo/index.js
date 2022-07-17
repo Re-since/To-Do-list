3c11974d7786c9ddb78d99abd6ae69026a2654d6
Resince <1876702860@qq.com>
2022-07-18 00:17:44 +0800

    To Do List first commit

--- src/Todo/index.js
+++ src/Todo/index.js
@@ -1,5 +1,40 @@
+import { useStore } from '../store'
+import { observer } from 'mobx-react-lite'
+import uuid from 'react-uuid'
+import { useState } from 'react'
 import './index.css'
+
 function Task() {
+  const { taskStore } = useStore();
+
+  function singleCheck(id) {
+    taskStore.singleCheck(id);
+  }
+
+  function multiCheck(e) {
+    taskStore.multiCheck(e.target.checked)
+  }
+
+  function deleteTask(id) {
+    taskStore.deleteTask(id);
+  }
+
+  const [text, setText] = useState('');
+  function addTask(e) {
+    if (e.keyCode === 13) {
+
+      taskStore.addTask(
+        {
+          id: uuid(),
+          name: text,
+          isDone: false
+        }
+      )
+
+      setText('');
+    }
+  }
+
   return (
     <section className="todoapp">
       <header className="header">
@@ -9,6 +44,9 @@ function Task() {
           autoFocus
           autoComplete="off"
           placeholder="What needs to be done?"
+          value={text}
+          onChange={(e) => { setText(e.target.value) }}
+          onKeyUp={addTask}
         />
       </header>
       <section className="main">
@@ -16,31 +54,42 @@ function Task() {
           id="toggle-all"
           className="toggle-all"
           type="checkbox"
+          checked={taskStore.isAll}
+          onChange={multiCheck}
         />
         <label htmlFor="toggle-all"></label>
         <ul className="todo-list">
-          <li
-            className="todo"
-          >
-            <div className="view">
-              <input className="toggle" type="checkbox" />
-              <label >learn react</label>
-              <button className="destroy"></button>
-            </div>
-          </li>
-          <li
-            className="todo completed"
-          >
-            <div className="view">
-              <input className="toggle" type="checkbox" defaultChecked={true}/>
-              <label >learn react</label>
-              <button className="destroy"></button>
-            </div>
-          </li>
+          {
+            taskStore.list.map(item => (
+              <li
+                key={item.id}
+                className={item.isDone ? "to do completed" : "to do"}
+              >
+                <div className="view">
+                  <input
+                    className="toggle"
+                    type="checkbox"
+                    onChange={() => singleCheck(item.id)}
+                    checked={item.isDone}
+                  />
+                  <label >{item.name}</label>
+                  <button
+                    className="destroy"
+                    onClick={() => deleteTask(item.id)}
+                  ></button>
+                </div>
+              </li>
+            ))
+          }
         </ul>
       </section>
+      <footer className='footer'>
+        <span className='todo-count'>
+          Completed Tasks: {taskStore.finishTaskNumber} -- All Tasks: {taskStore.list.length}
+        </span>
+      </footer>
     </section>
   )
 }
 
-export default Task
+export default observer(Task)
--- src/store/task.Store.js
+++ src/store/task.Store.js
@@ -1,14 +1,14 @@
 
-import {  makeAutoObservable } from 'mobx'
+import { makeAutoObservable } from 'mobx'
 class TaskStore {
   list = [
     {
-      id:1,
+      id: 1,
       name: '学习react',
       isDone: true
     },
     {
-      id:2,
+      id: 2,
       name: '搞定mobx',
       isDone: true
     }
@@ -16,5 +16,34 @@ class TaskStore {
   constructor() {
     makeAutoObservable(this)
   }
+
+  singleCheck(id) {
+    const item = this.list.find(item => item.id === id)
+    item.isDone = !item.isDone
+  }
+
+  multiCheck(checked) {
+    // console.log(checked);
+    this.list.forEach(item => {
+      item.isDone = checked;
+    })
+  }
+
+  get isAll() {
+    return this.list.every(item => item.isDone)
+  }
+
+  deleteTask(id) {
+    this.list = this.list.filter(item => item.id !== id);
+  }
+
+  addTask(task) {
+    this.list.push(task);
+  }
+
+  get finishTaskNumber() {
+    return this.list.filter(item => item.isDone !== true).length;
+  }
 }
+
 export default TaskStore
